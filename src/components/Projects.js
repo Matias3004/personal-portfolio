741681b2e2ab85711ec53ff714f30153a47550f8
Mateusz Muzioł <matias3004@gmail.com>
2023-02-23 19:46:34 +0100

    Initial Commit

---
+++ src/components/Projects.js
@@ -0,0 +1,23 @@
+export const Projects = () => {
+    const projects = [
+        {
+            title: "Cognitive Proccess",
+            description: "Charnel's EP",
+            imgUrl: projImg1,
+        },
+        {
+            title: "Tales From The Nightbutcher's House",
+            description: "MosherZ's first LP",
+            imgUrl: projImg2,
+        },
+        {
+            title: "ReGenesis"
+        }
+    ];
+
+    return (
+        <section>
+            
+        </section>
+    )
+}
