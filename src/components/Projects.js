636ebcddb61eeb7ed9b3ca14f860b8e047a44ce7
Matias3004 <matias3004@gmail.com>
2023-02-23 20:20:01 +0100

    Added project images

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,18 +1,15 @@
 export const Projects = () => {
     const projects = [
         {
-            title: "Cognitive Proccess",
-            description: "Charnel's EP",
-            imgUrl: projImg1,
-        },
-        {
             title: "Tales From The Nightbutcher's House",
             description: "MosherZ's first LP",
-            imgUrl: projImg2,
+            imgUrl: tales,
         },
         {
-            title: "ReGenesis"
-        }
+            title: "ReGenesis",
+            description: "Moyra's second EP",
+            imgUrl: regenesis,
+        },
     ];
 
     return (
